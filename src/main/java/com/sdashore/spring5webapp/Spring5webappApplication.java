f8895c1ae088b617ea3a3b1ca806cad1cad8b079
shapan Dashore <shapan@shapans-MacBook-Pro.local>
2019-06-30 14:51:05 -0400

    add thyeleaf template to view all books

--- src/main/java/com/sdashore/spring5webapp/Spring5webappApplication.java
+++ src/main/java/com/sdashore/spring5webapp/Spring5webappApplication.java
@@ -1,9 +1,11 @@
 package com.sdashore.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sdashore/spring5webapp/controllers/BookController.java
+++ src/main/java/com/sdashore/spring5webapp/controllers/BookController.java
@@ -1,5 +1,24 @@
 package com.sdashore.spring5webapp.controllers;
 
+import com.sdashore.spring5webapp.model.Book;
+import com.sdashore.spring5webapp.model.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BookController {
 
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
 }
